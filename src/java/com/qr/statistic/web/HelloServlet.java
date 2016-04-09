127dd32edcf7fb2955195b80a9296fd36668d30e
chenjun <chenjun@rd.netease.com>
2016-04-09 11:43:23 +0800

    FIX: init project

---
+++ src/java/com/qr/statistic/web/HelloServlet.java
@@ -0,0 +1,41 @@
+/**
+ * @(#)HelloServlet, 2016/4/9.
+ * <p/>
+ * Copyright 2016 Youdao, Inc. All rights reserved.
+ * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.qr.statistic.web;
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by chenjun on 2016/4/9.
+ */
+public class HelloServlet extends HttpServlet{
+    private static Logger LOG = Logger.getLogger(HelloServlet.class);
+
+    protected void doGet(HttpServletRequest request,
+        HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        LOG.info("headers=" + request.getHeader("User-agent"));
+        LOG.info("Hello I,m hello");
+
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<body>");
+        out.println("Hello World!!");
+        out.println("</body>");
+        out.println("</head>");
+        out.println("</html>");
+        out.close();
+    }
+}
---
+++ src/java/com/qr/statistic/web/QrStatisticContextListener.java
@@ -0,0 +1,41 @@
+/**
+ * @(#)QrStatisticContextListener, 2016/4/9.
+ * <p/>
+ * Copyright 2016 Youdao, Inc. All rights reserved.
+ * YOUDAO PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.qr.statistic.web;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import java.io.File;
+
+/**
+ * Created by chenjun on 2016/4/9.
+ */
+public class QrStatisticContextListener implements ServletContextListener{
+    
+    private static Logger LOG = Logger.getLogger(QrStatisticContextListener.class);
+    
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        String realPath = servletContextEvent.getServletContext().getRealPath("WEB-INF");
+        File confFile = new File(realPath, "conf");
+        System.out.println("config path: " + confFile.getAbsolutePath());
+        System.err.println("config path: " + confFile.getAbsolutePath());
+
+        //1. load log4j
+        File log4jFile = new File(confFile, "log4j.properties");
+        PropertyConfigurator.configureAndWatch(log4jFile.getAbsolutePath());
+        LOG.info("config path: " + confFile.getAbsolutePath());
+    }
+}
